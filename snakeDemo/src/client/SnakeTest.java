bbeb4941b4179595ec98c48fc57f1fa81c3a8c66
Joon Ki Choi <j.choi@fadendaten.ch>
2016-12-03 19:15:27 +0100

    add Kristina's files uploaded to Google Drive

--- snakeDemo/src/client/SnakeTest.java
+++ snakeDemo/src/client/SnakeTest.java
@@ -4,6 +4,7 @@ import processing.core.PApplet;
 import processing.core.PVector;
 import processing.event.KeyEvent;
 import shared.Snake;
+import shared.Food;
 
 enum Rotation {
     NONE, LEFT, RIGHT
@@ -18,6 +19,7 @@ public class SnakeTest extends PApplet {
     static PVector direction = new PVector(1,0);
     static Rotation rotation = Rotation.NONE;
     static Snake snake = new Snake(100,100);
+    private Food food = null; //FOOD
 
     public static void main(String[] args)  {
         PApplet.main("client.SnakeTest");
@@ -36,9 +38,35 @@ public class SnakeTest extends PApplet {
             ellipse(v.x, v.y, 20,20);
         }
     }
+    
+    //FOOD
+    public void setFood() { 
+        this.food = new Food(SCREEN_X, SCREEN_Y);
+    }
+    
+    public void drawFood() {
+    	if (this.food != null) {
+    		ellipse(this.food.getX(), this.food.getY(), 10,10);
+    	}
+    }
+    //FOOD
+    // checks if the snake is close enough to eat the food
+    public Boolean checkFoodProximity(float snakeInt, float foodInt) {
+    	int s = (int)snakeInt;
+    	int f = (int)foodInt;
+    	int closerThan = 10;
+    	
+    	return (Math.abs((long)(s - f)) <= closerThan);
+    }
+    
+    //FOOD
+    public Boolean isEaten() {
+    	return (checkFoodProximity(snake.head().x, food.getX()) && checkFoodProximity(snake.head().y, food.getY()));
+    }
+    
 
     @Override
-    public void draw() {
+    public void draw() {    	
         background(255);
 
         if (rotation == Rotation.LEFT) {
@@ -49,6 +77,25 @@ public class SnakeTest extends PApplet {
 
         snake.moveBy(direction);
         drawSnake(snake);
+        
+        
+        //FOOD
+        if (food == null) {
+        	setFood();
+        	//System.out.println(food.getId());
+        } else { 
+        	if (isEaten()) {
+        		//System.out.println("eaten");
+        		snake.grow(2);
+        		setFood();
+        		//System.out.println(food.getId());
+        	} else {
+        	drawFood();
+        	}
+        }
+        
+        
+        
     }
 
     @Override
