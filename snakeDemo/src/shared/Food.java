89918c1c9d83dac98d89d4b827e1ae6900f600cf
Jean-Pierre Höhmann <jp@hhmn.de>
2016-12-10 02:20:42 +0100

    Fix type of shared.Food().

--- snakeDemo/src/shared/Food.java
+++ snakeDemo/src/shared/Food.java
@@ -8,7 +8,7 @@ public class Food extends PVector {
     static final Random random = new Random();
     private static final long serialVersionUID = 43L;
 
-    public Food(int x, int y) { super(x, y); }
+    public Food(float x, float y) { super(x, y); }
 
     public static Food randomFood(int fieldWidth, int fieldHeight) {
         return new Food(
